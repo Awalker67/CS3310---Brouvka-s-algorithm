62d51cd61661459f7be4df6754110a74251beab2
Chris Tcherkezian <43045512+christcherkezian@users.noreply.github.com>
2021-05-02 22:58:12 -0700

    Add files via upload
    Updated

--- MST.py
+++ MST.py
@@ -5,12 +5,12 @@ class Graph:
     def __init__(self, nodes):
         self.v = nodes # Indicates the number of nodes from parameter
         self.edges = [] #Initialize empy edges list
-        self.component = {} #Initializes empty dictionary
+        self.component = {} #Initializes empty dictionary to store index a node's encompassing component
 
     def add_edge(self, u, v, weight):   #Function to add edge with connecting nodes and a weight
         self.edges.append([u, v, weight])    
 
-    def find_component(self, u):  #Recursion to find component of node u
+    def find_component(self, u):  #Recursion to find component of node u until finding the node
         if self.component[u] == u:
             return u
         return self.find_component(self.component[u])
@@ -23,23 +23,23 @@ class Graph:
                 self.component[i] = self.find_component(i)
 
 
-    def union(self, component, u, v):
-        if component[u] <= component[v]:
-            self.component[u] = v
-            component[v] += component[u]
+    def union(self, component, u, v):       #Method to unify two components into one given two nodes
+        if component[u] <= component[v]:    #Checks if first component has less/equal nodes than the first
+            self.component[u] = v           #Smaller components is added to the larger
+            component[v] += component[u]    #Smaller components is added to the larger
             self.set_component(u)
 
-        elif component[u] >= component[v]:
+        elif component[u] > component[v]:  #Else if first component has more nodes than the first
             self.component[v] = self.find_component(u)
             component[u] += component[v]
             self.set_component(v)
 
     def boruvka(self):
-        component = []
+        component = []      #Initialize empty list of components
         weight = 0
-        minimum_weight_edge = [-1] * self.v
+        minimum_weight_edge = [-1] * self.v        #Initialize min edge with -1 by default
 
-        for node in range(self.v):
+        for node in range(self.v):      #Find roots of components on both sides of the edge
             self.component.update({node: node})
             component.append(1)
 
@@ -57,15 +57,15 @@ class Graph:
                 v_component = self.component[v]
 
                 if u_component != v_component:
-                    if minimum_weight_edge[u_component] == -1 or \
-                            minimum_weight_edge[u_component][2] > w:
+                    # Checks if 1st edge's weight doesn't exist or is grather than 2nd edge's weight, then assign the 2nd weight to the 1st
+                    if minimum_weight_edge[u_component] == -1 or minimum_weight_edge[u_component][2] > w:
                         minimum_weight_edge[u_component] = [u, v, w]
-                    if minimum_weight_edge[v_component] == -1 or \
-                            minimum_weight_edge[v_component][2] > w:
+                    # Checks if 2nd edge's weight doesn't exist or is grather than 1st edge's weight, then assign the 1st weight to the second
+                    if minimum_weight_edge[v_component] == -1 or minimum_weight_edge[v_component][2] > w:
                         minimum_weight_edge[v_component] = [u, v, w]
 
             for node in range(self.v):
-                if minimum_weight_edge[node] != -1:
+                if minimum_weight_edge[node] != -1:     #Once minium weight edge is found, add the edge to the tree
                     u = minimum_weight_edge[node][0]
                     v = minimum_weight_edge[node][1]
                     w = minimum_weight_edge[node][2]
@@ -79,9 +79,9 @@ class Graph:
                         print("Added edge [" + str(u) + " - "
                               + str(v) + "]"
                               + " With weight: " + str(w))
-                        num_of_components -= 1
+                        num_of_components -= 1          #Reduce the total number of components when unionizing
 
-            minimum_weight_edge = [-1] * self.v
+            minimum_weight_edge = [-1] * self.v    #Set list back to -1 to reset
         print("----------------------------------")
         print("The total weight of the minimal spanning tree is: " + str(weight) +"\n")
 
